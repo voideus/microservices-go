73cd858c138b308ffd5dfc800242111fec7f305c
voideus <tbikash227@gmail.com>
2022-08-15 15:29:04 +0545

    Initial commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/voideus/microservices-go/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	// hh := handlers.NewHello(l)
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	s := &http.Server{
+		Addr:         ":5000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
